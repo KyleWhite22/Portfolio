5f1db9016566dcd9ce33e68ddbcda675cc645a83
KyleWhite22 <kewhite900@gmail.com>
2025-07-28 20:35:03 -0400

    steam login api url corrected

--- client/src/pages/GameAILogin.jsx
+++ client/src/pages/GameAILogin.jsx
@@ -5,7 +5,7 @@ function GameAILogin() {
         <div className="home-container">
                 <h1>GameTracker AI</h1>
             <p>Your personal video game journal and AI assistant.</p>
-                <a href="http://localhost:5000/auth/steam">
+                <a href={`${import.meta.env.VITE_API_URL}/auth/steam`}>
         <button className="steam-login-btn">Log in with Steam</button>
       </a>
         </div>
