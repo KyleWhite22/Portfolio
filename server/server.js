f7b0613c967cf6857a44fb6c5b198bd9a973f1bb
KyleWhite22 <kewhite900@gmail.com>
2025-08-05 21:03:03 -0400

    updated cookie config to hanlde both devleopment and prod

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-  secure: true,         
+  secure: process.env.NODE_ENV === 'production',         
   sameSite: 'none',     
   httpOnly: true,
   maxAge: 24 * 60 * 60 * 1000
