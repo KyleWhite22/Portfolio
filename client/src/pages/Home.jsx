65875d4e11e50bba5d8d55877e883969ea348a81
KyleWhite22 <kewhite900@gmail.com>
2025-08-15 14:42:38 -0400

    fixed homepage styling

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,22 +5,23 @@ import kyleImg from '../assets/kyle.jpeg';
 function Home() {
   return (
     <div className="home-wrapper">
-      <header className="top-nav">
-        <nav className="nav-links">
-          <Link to="/personal" className="nav-link">Personal</Link>
-          <Link to="/education" className="nav-link">Education</Link>
-          <Link to="/work" className="nav-link">Work</Link>
-          <Link to="/GameAILogin" className="nav-link">GameBuddyAI</Link>
-        </nav>
-      </header>
+      <div className="center">   {/* NEW */}
+        <header className="top-nav">
+          <nav className="nav-links">
+            <Link to="/personal" className="nav-link">Personal</Link>
+            <Link to="/education" className="nav-link">Education</Link>
+            <Link to="/work" className="nav-link">Work</Link>
+            <Link to="/GameAILogin" className="nav-link">GameBuddyAI</Link>
+          </nav>
+        </header>
 
-      <div className="home-content">
-        <img src={kyleImg} alt="Kyle White" className="avatar" />
-        <h1 className="name">Kyle White</h1>
-        <p className="tagline">Welcome to my site!</p>
+        <div className="home-content">
+          <img src={kyleImg} alt="Kyle White" className="avatar" />
+          <h1 className="name">Kyle White</h1>
+          <p className="tagline">Welcome to my site!</p>
+        </div>
       </div>
     </div>
   );
 }
-
 export default Home;
